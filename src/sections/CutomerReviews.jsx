a68da6e085ed3e81299f17ac9309a6d1003773a0
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-10-25 18:58:46 +0600

    start

---
+++ src/sections/CutomerReviews.jsx
@@ -0,0 +1,5 @@
+const CutomerReviews = () => {
+  return <div>CutomerReviews</div>;
+};
+
+export default CutomerReviews;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import CutomerReviews from "./CutomerReviews";
+import PopularProduct from "./PopularProduct";
+
+export {
+  Hero,
+  Footer,
+  Services,
+  Subscribe,
+  SpecialOffer,
+  SuperQuality,
+  CutomerReviews,
+  PopularProduct,
+};
