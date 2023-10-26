26dd1e59b24999bd3885b3e563e782e582a82e45
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-10-26 14:16:57 +0600

    definitely over

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         <p className="text-xl font-montserrat text-coral-red">
           Our summer collention
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
@@ -52,7 +52,7 @@ const Hero = () => {
           className="object-contain relative z-10"
         />
 
-        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-xl:-bottom-[35%] max-sm:px-6">
           {shoes.map((shoe, index) => (
             <div key={index}>
               <ShoeCard
